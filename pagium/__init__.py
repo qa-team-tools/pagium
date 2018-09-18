82f63aa71b9f6539e885228415103a25ae7a97c0
trifonovmixail <trifonov.net@gmail.com>
2018-09-18 20:22:12 +0300

    version 0.0.5

--- pagium/__init__.py
+++ pagium/__init__.py
@@ -11,7 +11,7 @@ from pagium.page import Page, PageElement, WebElement
 from pagium.webdriver import Remote, Chrome, Firefox, PhantomJS, Opera, Safari
 
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 
 
 __all__ = [
