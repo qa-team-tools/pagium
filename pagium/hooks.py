ef01fbeefa8d7819d18e31398f61cf9aaeb48069
Trifonov Mikhail <mtrifonov@ozon.travel>
2019-06-07 14:57:40 +0300

    add hooks module

---
+++ pagium/hooks.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from selenium.webdriver.common.keys import Keys
+
+from pagium.page import LazyWebElement
+
+
+class BasePageElementHook:
+
+    def __init__(self, web_element: LazyWebElement):
+        self.web_element = web_element
+
+
+class ClickActionHook(BasePageElementHook):
+
+    def __call__(self):
+        self.web_element.refresh()
+        self.web_element.click()
+
+
+class SendEnterHook(BasePageElementHook):
+
+    def __call__(self, text=None):
+        if text in None:
+            text = Keys.ENTER
+        else:
+            text = text + Keys.ENTER
+
+        self.web_element.send_keys(*text)
