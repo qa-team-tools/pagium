3c12d30c6560199893b3160cd1ee03906ac995f2
trifonovmixail <trifonov.net@gmail.com>
2019-06-14 12:39:57 +0300

    add tests to hooks and fix SendEnterHook

--- pagium/hooks.py
+++ pagium/hooks.py
@@ -15,15 +15,57 @@ class BasePageElementHook:
 
 
 class ClickActionHook(BasePageElementHook):
+    """
+    >>> from pagium.webdriver import Remote
+    >>> from pagium.page import PageElement, Page
+    >>> from selenium.webdriver.common.by import By
+
+    >>> wd = Remote(
+    ... command_executor='http://localhost:4444/wd/hub',
+    ... desired_capabilities={'browserName': 'chrome'},
+    ... polling_timeout=10,
+    ... polling_delay=0.5,
+    ... )
+
+    >>> class GooglePageTestBasic(Page):
+    ...     search = PageElement(by=By.NAME, value='q')
+    ...     submit = PageElement(by=By.NAME, value='btnK', hook=ClickActionHook)
+
+    >>> with GooglePageTestBasic(wd, 'https://google.com') as page:
+    ...     page.search.send_keys(*'python selenium')
+    ...     page.submit()
+
+    >>> wd.quit()
+    """
 
     def __call__(self):
         self.web_element.click()
 
 
 class SendEnterHook(BasePageElementHook):
+    """
+    >>> from pagium.webdriver import Remote
+    >>> from pagium.page import PageElement, Page
+    >>> from selenium.webdriver.common.by import By
+
+    >>> wd = Remote(
+    ... command_executor='http://localhost:4444/wd/hub',
+    ... desired_capabilities={'browserName': 'chrome'},
+    ... polling_timeout=10,
+    ... polling_delay=0.5,
+    ... )
+
+    >>> class GooglePageTestBasic(Page):
+    ...     search = PageElement(by=By.NAME, value='q', hook=SendEnterHook)
+
+    >>> with GooglePageTestBasic(wd, 'https://google.com') as page:
+    ...     page.search('python selenium')
+
+    >>> wd.quit()
+    """
 
     def __call__(self, text=None):
-        if text in None:
+        if text is None:
             text = Keys.ENTER
         else:
             text = text + Keys.ENTER
